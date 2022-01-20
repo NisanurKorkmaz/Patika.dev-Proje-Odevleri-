26e224fdd6f7666ea9d7c5bd33065a6f50d0b07c
Nisanur Korkmaz <80275552+NisanurKorkmaz@users.noreply.github.com>
2022-01-20 10:31:45 +0300

    Update odev.py

--- PYTHON/odev.py
+++ PYTHON/odev.py
@@ -13,5 +13,29 @@ for sublist in l:
     
 print(new_list)
 
+
+
+
 #SORU 2 
+
+l2 = [[1, 2], [3, 4], [5, 6, 7]]
+
+liste_uzunluk=len(l2)
+
+yeni_liste=[]
+
+  
+def Ters(lst):
+    lst.reverse()
+    return lst
+
+
+
+while(liste_uzunluk>0):
+    tersi=Ters(l2[liste_uzunluk-1])
+    yeni_liste.append(tersi)
+    liste_uzunluk=liste_uzunluk-1
+
+
+print(yeni_liste)
 
