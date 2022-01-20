6d398786b2ed7f10e0ddc9a80b7363128e5fa5ba
Nisanur Korkmaz <80275552+NisanurKorkmaz@users.noreply.github.com>
2022-01-20 10:12:48 +0300

    Add files via upload

---
+++ PYTHON/odev.py
@@ -0,0 +1,13 @@
+
+l = [[1,'a',['cat'],2],[[[3]],'dog'],[4,5]]
+
+new_list = []
+
+for sublist in l:
+    for item in sublist:
+        #print(item)
+        new_list.append(item)
+
+
+    
+print(new_list)
